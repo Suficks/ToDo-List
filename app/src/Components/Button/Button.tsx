0d8a7fc33966505d98cdb48f8a8caff02c3fbc37
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-11-29 17:50:12 +0300

    feat: add button component

--- app/src/Components/Button/Button.tsx
+++ app/src/Components/Button/Button.tsx
@@ -1,5 +1,10 @@
-const Button = () => {
+import React from 'react';
+import cn from './button.module.scss';
 
+const Button = ({ className }) => {
+  return (
+    <button className={`${className} ${cn.button}`}></button>
+  )
 }
 
 export default Button
