5345258e057fbdc9ff318e42904575ee1379cf7a
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-11-30 19:25:47 +0300

    feat: add custom select

--- app/src/Components/Select/Select.tsx
+++ app/src/Components/Select/Select.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cn from './select.module.scss';
 import { ReactComponent as Arrow } from '../../Assets/arrow-icon.svg';
 
 const Select = () => {
+  const [selected, SetSelected] = useState(false);
+
   return (
-    <div className={cn.select}>
-      <div className={cn.options}>
-        <p className={cn.item}>ALL</p>
-        <p className={cn.item}>DONE</p>
-        <p className={cn.item}>ACTIVE</p>
-      </div>
-      <Arrow className={cn.arrow} />
+    <div className={cn.select} onClick={() => SetSelected(!selected)}>
+      <p className={cn.selected}>ALL</p>
+      <ul className={`${cn.options} ${selected ? cn.options_show : ''}`}>
+        <li className={`${cn.item} ${selected ? cn.item_show : ''}`}>All</li>
+        <li className={`${cn.item} ${selected ? cn.item_show : ''}`}>Complete</li>
+        <li className={`${cn.item} ${selected ? cn.item_show : ''}`}>Incomplete</li>
+      </ul>
+      <Arrow className={`${cn.arrow} ${selected ? cn.rotate : ''}`} />
     </div>
   )
 }
