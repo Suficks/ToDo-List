fc34e7c57a022eb02146118a7dd575109838769c
Sofia Vdovichenko <vdo-sofia@mail.ru>
2023-11-30 23:40:59 +0300

    feat: add task component

---
+++ app/src/Components/Task/Task.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import cn from './task.module.scss';
+
+const Task = () => {
+  return (
+    <div className={cn.task}>
+      <label className={cn.label}>
+        <input type="checkbox" className={cn.checkbox} />
+        <span className={cn.fake}></span>
+      </label>
+      <p className={cn.text}>SDSLDlslalk</p>
+      <div className={cn.btn_container}>
+        <button className={cn.edit}></button>
+        <button className={cn.delete}></button>
+      </div>
+    </div>
+  )
+}
+
+export default Task
